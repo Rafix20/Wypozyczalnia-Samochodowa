77537c1f65e22ecefeef24f558040b24b005198a
SWIDERTOKOCUR <62543579+SWIDERTOKOCUR@users.noreply.github.com>
2021-11-25 14:12:39 +0100

    Create klienci.java

---
+++ klienci.java
@@ -0,0 +1,15 @@
+public class klienci {
+    public static String adres;
+    public static int kod_pocztowy;
+    public static String imie;
+    public static String nazwisko;
+    public static int wiek;
+
+    public klienci(String adres, int kod_pocztowy, String imie, String nazwisko, int wiek) {
+        this.adres = adres;
+        this.kod_pocztowy = kod_pocztowy;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+}
