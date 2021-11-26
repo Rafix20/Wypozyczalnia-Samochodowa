890dd0c934b8f4ea25a98f9ff1adcf8546bfafcc
Rafix <62878878+Rafix20@users.noreply.github.com>
2021-11-26 13:43:58 +0000

    Update klienci.java

--- klienci.java
+++ klienci.java
@@ -1,15 +1,16 @@
 public class klienci {
-    public static String adres;
-    public static int kod_pocztowy;
-    public static String imie;
-    public static String nazwisko;
-    public static int wiek;
+    private String adres_k;
+    private int kod_pocztowy_k;
+    private String imie_k;
+    private String nazwisko_k;
+    private int wiek_k;
 
-    public klienci(String adres, int kod_pocztowy, String imie, String nazwisko, int wiek) {
-        this.adres = adres;
-        this.kod_pocztowy = kod_pocztowy;
-        this.imie = imie;
-        this.nazwisko = nazwisko;
-        this.wiek = wiek;
+    public klienci(String adres_k, int kod_pocztowy_k, String imie_k, String nazwisko_k, int wiek_k) {
+        this.adres_k = adres_k;
+        this.kod_pocztowy_k = kod_pocztowy_k;
+        this.imie_k = imie_k;
+        this.nazwisko_k = nazwisko_k;
+        this.wiek_k = wiek_k;
     }
 }
+
